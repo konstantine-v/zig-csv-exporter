c9cc862c53179f6adc4c2d43b081642821c13003
KonstantineV <konstantinev@protonmail.com>
2024-11-19 13:10:25 -0600

    init

---
+++ src/main.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("Let's get started...\n", .{});
+
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
+
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    // try bw.flush(); // don't forget to flush!
+}
